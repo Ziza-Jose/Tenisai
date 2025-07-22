f7f5fc968122ac18a6cf93532c3da00c72df948c
Ziza-Jose <zizacaquarta@gmail.com>
2025-07-22 21:58:38 +0100

    main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Bem-vindo ao TenisAI!</h1>
-  <p>A previsão mais inteligente de resultados de tênis.</p>
+  <p>Bem-vindo ao TenisAI” → “Nova versão TenisAI 2025”).</p>
 `;
